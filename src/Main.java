27456fed6d0c55cb16aa06d7d216792fe83ffe03
amin <aminahissa8@gmail.com>
2025-12-18 11:03:13 +0300

    class w

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        System.out.println("amin");
+        int i =0 ;
+        System.out.println("hello");
 
     }
 }
